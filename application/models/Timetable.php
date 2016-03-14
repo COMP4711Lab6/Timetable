209711044be3e27baf52b659585ca0cd608e7fde
Ricky Chen <rickyshen1991@hotmail.com>
2016-03-14 16:31:49 -0700

    fixed dtd and data xml

--- application/models/Timetable.php
+++ application/models/Timetable.php
@@ -6,15 +6,7 @@
  * Time: 9:59 AM
  */
 class Timetable extends CI_Model{
-    protected $xml = null;
-    protected $courses = array();
-    protected $periods = array();
-    protected $days = array();
 
-    public function  __construct()
-    {
-        parent::construct();
-        $this -> xml = simplexml_load_file(DATAPATH,"courses.xml");
 
-    }
+
 }
